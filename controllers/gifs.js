ac5673edba0285ca6cb227adcb1cb4e814295224
Minhquan Thai <minhquanthai@gmail.com>
2018-04-13 14:39:36 -0400

    Created index route

--- controllers/gifs.js
+++ controllers/gifs.js
@@ -2,4 +2,8 @@ const express = require('express')
 const router = express.Router()
 const Gifs = require('../models/gifs')
 
+router.get('/', (req, res) => {
+  Gifs.find({}).then(gifs => res.render('index', {gifs}))
+})
+
 module.exports = router
