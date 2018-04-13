367909d532f464d80fd13948fc0bc4779ec10235
Minhquan Thai <minhquanthai@gmail.com>
2018-04-13 14:28:28 -0400

    Set up gif controller

---
+++ controllers/gifs.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const Gifs = require('../models/gifs')
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const gifsController = require('./controllers/gifs')
 
 app.set('view engine', 'hbs')
+
+app.use('/', gifsController)
 
 app.listen(3000, () => console.log('port 3000'))
