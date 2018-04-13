0402c3bff4fa406a2667c28142b00e87f1937562
Minhquan Thai <minhquanthai@gmail.com>
2018-04-13 14:06:26 -0400

    Defined gif model

---
+++ models/gifs.js
@@ -0,0 +1,11 @@
+const mongoose = require('../db/connection')
+
+const GifSchema = new mongoose.Schema({
+  url: String,
+  title: String,
+  tags: [String]
+})
+
+const Gifs = mongoose.model('Gifs', GifSchema)
+
+module.exports = Gifs
