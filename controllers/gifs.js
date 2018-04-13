db4d8831960d0d663a578f8750d07ae145d81000
Minhquan Thai <minhquanthai@gmail.com>
2018-04-13 15:17:26 -0400

    Created NEW route

--- controllers/gifs.js
+++ controllers/gifs.js
@@ -7,6 +7,15 @@ router.get('/', (req, res) => {
   Gifs.find({}).then(gifs => res.render('index', {gifs}))
 })
 
+// new route
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  Gifs.create(req.body).then(gif => res.redirect('/'))
+})
+
 // show route
 router.get('/:id', (req, res) => {
   Gifs.findOne({_id: req.params.id}).then(gif => res.render('show', {gif}))
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const hbs = require('hbs')
+const parser = require('body-parser')
+
 const app = express()
+
 const gifsController = require('./controllers/gifs')
 
 app.set('view engine', 'hbs')
+app.use(parser.urlencoded({extended: true}))
 
 app.use('/', gifsController)
 
