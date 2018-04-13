d7fa492e49d46a944f2d7a10e2deeffb25762560
Minhquan Thai <minhquanthai@gmail.com>
2018-04-13 15:02:26 -0400

    Created show route

--- controllers/gifs.js
+++ controllers/gifs.js
@@ -2,8 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Gifs = require('../models/gifs')
 
+// index route
 router.get('/', (req, res) => {
   Gifs.find({}).then(gifs => res.render('index', {gifs}))
+})
+
+// show route
+router.get('/:id', (req, res) => {
+  Gifs.findOne({_id: req.params.id}).then(gif => res.render('show', {gif}))
 })
 
 module.exports = router
