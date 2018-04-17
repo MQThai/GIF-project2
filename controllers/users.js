7aac08f7ac5747b50413bf305cfc4002844e9062
Minhquan Thai <minhquanthai@gmail.com>
2018-04-17 10:12:00 -0400

    Made signup page and added Flash messages

--- controllers/users.js
+++ controllers/users.js
@@ -4,9 +4,17 @@ const methodOverride = require('method-override')
 const passport = require('passport')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.get('/signup', (req, res) => {
+  res.render('signup.hbs', {message: req.flash('signupMessage')})
+})
+
 router.post('/signup', (req, res) => {
   const signupStrategy = passport.authenticate('local-signup', {
-    successRedirect: '/'
+    successRedirect: '/',
     failureRedirect: '/signup',
     failureFlash: true
   })
