6200f5e12f12af283d0d0faed19beef9b7cf9689
Minhquan Thai <minhquanthai@gmail.com>
2018-04-18 13:53:41 -0400

    Set up mlab db

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/giflibrary')
+if (process.env.NODE_ENV == 'production') {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/giflibrary')
+}
 
 mongoose.Promise = Promise
 
